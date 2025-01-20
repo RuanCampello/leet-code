f45137cddecbb849fd55b6daf78eb4cc02c7840a
RuanCampello <ruancampello888@gmail.com>
2025-01-20 15:49:44 -0300

    add letter combination brute force

---
+++ 17.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+pub fn letter_combinations(digits: String) -> Vec<String> {
+    let mut combinations: Vec<String> = vec![String::new()];
+    
+    if digits.is_empty() {
+        return Vec::new();
+    }
+
+    let keyboard: HashMap<char, &str> = HashMap::from([
+        ('2', "abc"),
+        ('3', "def"),
+        ('4', "ghi"),
+        ('5', "jkl"),
+        ('6', "mno"),
+        ('7', "pqrs"),
+        ('8', "tuv"),
+        ('9', "wxyz"),
+    ]);
+    
+    for digit in digits.chars() {
+        if let Some(&letters) = keyboard.get(&digit) {
+            let mut new_combinations = Vec::new();
+
+            for combo in &combinations {
+                for letter in letters.chars() {
+                    let mut new_combo = combo.clone();
+                    new_combo.push(letter);
+                    new_combinations.push(new_combo);
+                }
+            }
+
+            combinations = new_combinations;
+        }
+    }
+
+    combinations
+}
+
+fn main() {
+    assert_eq!(
+        letter_combinations("23".into()),
+        vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    );
+    assert_eq!(letter_combinations("2".into()), vec!["a", "b", "c"]);
+    assert_eq!(
+        letter_combinations("22".into()),
+        vec!["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"]
+    );
+}
