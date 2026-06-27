2d63a85591750fbf90d4a2effbac8cdb31ab2467
RuanCampello <ruancampello999@gmail.com>
2026-06-27 08:35:02 -0300

    some python black magic

---
+++ 70.py
@@ -0,0 +1,23 @@
+from functools import cache
+
+
+@cache
+def climbStairs(n: int) -> int:
+    if n == 1 or n == 2:
+        return n
+
+    return climbStairs(n - 1) + climbStairs(n - 2)
+
+
+print(climbStairs(int(input())))
+
+# 4:
+# 1 - 1 - 1 - 1
+# 1 - 1 - 1 - 2
+# 1 - 1 - 2 - 1
+# 1 - 2 - 1 - 1
+# 2 - 1 - 1 - 1
+# 1 - 1 - 2
+# 1 - 2 - 1
+# 2 - 1 - 1
+# 2 - 2
